5fefbce3134bf8a80a9150e2bcfa538997f82f08
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2024-10-24 19:30:44 -0300

    Configuring eureka server

--- hr-eureka-server/src/test/java/com/rsfbernardes/hreurekaserver/HrEurekaServerApplicationTests.java
+++ hr-eureka-server/src/test/java/com/rsfbernardes/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -2,7 +2,9 @@ package com.rsfbernardes.hreurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootTest
 class HrEurekaServerApplicationTests {
 
