8250c12ac28d8a7c3ff513a9d80a345f8dd4225b
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2024-10-25 22:49:01 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/rsfbernardes/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.rsfbernardes.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.rsfbernardes.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(Long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+	
+}
