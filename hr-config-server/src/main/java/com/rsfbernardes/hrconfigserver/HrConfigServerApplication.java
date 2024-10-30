644f0e6fd08ff5031b7c3c62b014974de87437cf
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2024-10-29 21:53:19 -0300

    Private Git repository

--- hr-config-server/src/main/java/com/rsfbernardes/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/rsfbernardes/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,27 @@
 package com.rsfbernardes.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.stereotype.Component;
 
+@Component
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
+	
+	@Value("${spring.cloud.config.server.git.username}")
+	private String githubUser;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("USERNAME = " + githubUser);
 	}
 
 }
